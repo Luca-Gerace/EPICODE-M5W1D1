0440296418b20eb13f37ddc05ba808043f043904
Luca-Gerace <geraceluca91@gmail.com>
2024-05-23 19:24:41 +0200

    some fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-const Header = () => {
-    return (
-      <div>
-        some contents
-      </div>
-    );
+export default function Header() {
+  return (
+    <div className="flex ">
+      <div className="w-50 bg-[#000]"></div>
+      <div className="w-50 bg-[#000]"></div>
+    </div>
+  );
 }
 
-export default Header;
+// const Header = () => {
+//     return (
+//       <div className="flex ">
+//         <div className="w-50 bg-[#000]"></div>
+//         <div className="w-50 bg-[#000]"></div>
+//       </div>
+//     );
+// }
+
+// export default Header;
